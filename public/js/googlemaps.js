5f10a801fbc555a2b0d17e21a3f286f886c3a11c
Robert Yang & Viktor Nikitenko <pair+unknownbreaker.vnikiten01@devbootcamp.com>
2014-11-07 15:19:07 -0500

    google maps up and working

---
+++ public/js/googlemaps.js
@@ -0,0 +1,11 @@
+function initialize() {
+  var mapOptions = {
+    center: { lat: 40.70668, lng: -74},
+    zoom: 15
+
+  };
+  // var map = new google.maps.Map(document.getElementById('map-canvas'),
+  //   mapOptions);
+var map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
+}
