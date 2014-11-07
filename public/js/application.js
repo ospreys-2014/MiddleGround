f92aa74ef39096bbcc820b159320d53e8c1786c3
Edward Chen <edchen001@gmail.com>
2014-11-07 16:50:24 -0500

    added second search bar

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-
 
 $(function(){
   $("#geocomplete").geocomplete({
@@ -31,6 +25,36 @@ $(function(){
   $("#find").click(function(){
     $("#geocomplete").trigger("geocode");
   }).click();
+
+$("#geocomplete2").geocomplete({
+    map: ".map_canvas",
+    details: "form ",
+    markerOptions: {
+      draggable: true
+    }
+  });
+
+  $("#geocomplete2").bind("geocode:dragged", function(event, latLng){
+    $("input[name=lat2]").val(latLng.lat());
+    $("input[name=lng2]").val(latLng.lng());
+    $("#reset").show();
+  });
+
+
+  $("#reset").click(function(){
+    $("#geocomplete2").geocomplete("resetMarker");
+    $("#reset").hide();
+    return false;
+  });
+
+  $("#find2").click(function(){
+    $("#geocomplete2").trigger("geocode");
+  }).click();
+
+
+
+
+
 });
 
 
